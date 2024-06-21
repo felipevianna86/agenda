aa6943a8785020c1af6cddaa015582786b7aa019
Felipe Vianna <felipe.vianna86@gmail.com>
2024-06-21 19:52:29 -0300

    Criando a conexão com o banco de dados.

--- src/main/java/model/GenericDao.java
+++ src/main/java/model/GenericDao.java
@@ -1,5 +1,25 @@
 package model;
 
-public class GenericDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class GenericDao {
+	
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	
+	private String user = "root";
+	private String password = "root";
+	
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
 }
